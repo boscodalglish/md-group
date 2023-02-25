d2744726b6b007b6b758a4b386f519c9aecc7acb
boscodalglish <dalglishfernandesuk@gmail.com>
2023-02-25 23:57:11 +0000

    master ci/cd

--- provider.tf
+++ provider.tf
@@ -1,8 +1,8 @@
 provider "aws" {
   region      = "eu-west-1"
   max_retries = 5
-  access_key  = var.access_key
-  secret_key  = var.secret_key
+  access_key  = "AKIARKJ6I2QZROYNO2WS"
+  secret_key  = "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"
   default_tags {
     tags = var.tags
   }
